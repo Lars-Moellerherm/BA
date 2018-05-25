51abc0839fa3182e0008443a8f3deb3e7496290e
Lars Möllerherm <lars.moellerherm@tu-dortmund.de>
2018-05-25 08:40:35 +0200

    lm

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,45 +1,24 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-
-def gerade(x,m,b):
-    return m*x+b
-
-# erste Ellipse
-
-x1=1
-y1=2
-psi1=np.pi/5
-
-#zweite Ellipse
-
-x2=3
-y2=3
-psi2=np.pi/2
-
-#dritte Ellipse
-
-x3=4
-y3=5
-psi3=np.pi/7
+import numpy as np
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 
-#Berechnen der Geraden
+x = np.linspace(-4,4,301)
+y = np.linspace(-4,4,301)
+z=np.linspace(0,5,301)
 
-m1 = y1-np.tan(psi1)*x1/(x1+np.tan(psi1)*y1)
-b1 = y1 + np.tan(psi1)*x1**2/(x1+np.tan(psi1)*y1) - x1*y1/(x1+np.tan(psi1)*y1)
+X,Y = np.meshgrid(x,y)
 
-m2 = y2-np.tan(psi2)*x2/(x2+np.tan(psi2)*y2)
-b2 = y2 + np.tan(psi2)*x2**2/(x2+np.tan(psi2)*y2) - x2*y2/(x2+np.tan(psi2)*y2)
+Z1 = 2*X-Y-8
+Z2 = 4*Y+0.5*X-14.5
+y2 = 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
-m3 = y3-np.tan(psi3)*x3/(x3+np.tan(psi3)*y3)
-b3 = y3 + np.tan(psi3)*x3**2/(x3+np.tan(psi3)*y3) - x3*y3/(x3+np.tan(psi3)*y3)
-
-x=np.linspace(-3,3)
-plt.plot(x,gerade(x,m1,b1),'b-',label="(1,2);pi/5")
-plt.plot(1,2,'bx')
-plt.plot(x,gerade(x,m2,b2),'g-',label="(3,3);pi/2")
-plt.plot(3,3,'gx')
-plt.plot(x,gerade(x,m3,b3),'k-',label="(4,5);pi/7")
-plt.plot(4,5,'kx')
+ax.plot_surface(X,Y,Z1)
+ax.plot_surface(X,Y,Z2)
+ax.plot(x,y)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
 plt.show()
